6e57afd17189dae407aa0e732376763a05422d73
GabaeOh <rkqo4577@gmail.com>
2023-05-25 18:56:59 +0900

    ㄷ

---
+++ src/Eight.java
@@ -0,0 +1,12 @@
+import java.util.Scanner; 
+public class Eight {
+    public static void main(String[] args) {
+        @SuppressWarnings("resource") 
+        Scanner myObj = new Scanner(System.in);
+
+        if(String A = ""; a < 50;){
+            System.out.println(a +""+ "??!");
+        }
+    }
+    
+}
