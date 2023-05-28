1809c75c8b6ef83c84665e8ddb117209df6f5ea9
GabaeOh <rkqo4577@gmail.com>
2023-05-29 00:40:01 +0900

    28일

--- src/B_1000.java
+++ src/B_1000.java
@@ -1,13 +1,14 @@
-import java.util.Scanner; 
+import java.util.Scanner;
+
 public class B_1000 {
     public static void main(String[] args) {
-         // refer : https://www.acmicpc.net/problem/1000
+        // refer : https://www.acmicpc.net/problem/1000
         // 문제 : 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
         Scanner myObj = new Scanner(System.in);
 
         int A = 0;
         int B = 0;
-        A = myObj.nextInt(); 
+        A = myObj.nextInt();
         B = myObj.nextInt();
 
         System.out.println(A + B);
