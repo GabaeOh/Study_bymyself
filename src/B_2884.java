ab7660e08f4f662d221fd76983414e8240ef2257
GabaeOh <rkqo4577@gmail.com>
2023-05-31 10:45:56 +0900

    마무리

---
+++ src/B_2884.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class B_2884 {
+    public static void main(String[] args) {
+        // refer : https://www.acmicpc.net/problem/2884
+        /* 문제 :  "45분 일찍 알람 설정하기" 원래 설정되어 있는 알람을 45분 앞서는 시간으로 바꾸는 것이다.*/
+
+        Scanner myObj = new Scanner(System.in);
+        int H = myObj.nextInt();
+        int M = myObj.nextInt();
+
+        
+       
+    }
+}
