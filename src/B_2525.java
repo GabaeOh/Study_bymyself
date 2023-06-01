08414dc512fc159678da723cb7218a7cb146bdaa
GabaeOh <rkqo4577@gmail.com>
2023-06-02 01:00:34 +0900

    1일 백준

---
+++ src/B_2525.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class B_2525 {
+    public static void main(String[] args) {
+        // refer : https://www.acmicpc.net/problem/2525
+        /*
+         * 문제 : 훈제오리구이를 시작하는 시각과 오븐구이를 하는 데 필요한 시간이 분단위로 주어졌을 때,
+         * 오븐구이가 끝나는 시각을 계산하는 프로그램을 작성하시오.
+         */
+        Scanner myObj = new Scanner(System.in);
+
+        // 변수 선언
+        int A = myObj.nextInt();
+        int B = myObj.nextInt();
+        int C = myObj.nextInt();
+
+        B = B + C;
+
+        // '분' 조건 맞추기
+        if (B > 60) {
+            B = B - 60;
+            A = A + 1;
+            if (B == 60) {
+                B = 0;
+                A = A + 1;
+            }
+        } else if (B == 60) {
+            B = 0;
+            A = A + 1;
+        }
+
+        if (A == 24) {
+            A = 0;
+        }
+        System.out.println(A + " " + B);
+
+        myObj.close();
+    }
+}
--- src/B_2884.java
+++ src/B_2884.java
@@ -3,31 +3,48 @@ import java.util.Scanner;
 public class B_2884 {
     public static void main(String[] args) {
         // refer : https://www.acmicpc.net/problem/2884
-        /* 문제 : "45분 일찍 알람 설정하기" 
-                 원래 설정되어 있는 알람을 45분 앞서는 시간으로 바꾸는 것이다.*/
+        /*
+         * 문제 : "45분 일찍 알람 설정하기"
+         * 원래 설정되어 있는 알람을 45분 앞서는 시간으로 바꾸는 것이다.
+         */
 
         Scanner myObj = new Scanner(System.in);
         int H = myObj.nextInt();
         int M = myObj.nextInt();
 
-        if( H > 0 && M < 45 ){
-            int a = H-1;
-            int b = M+60-45;
-            System.out.print(a + " " + b);
+        if (M < 45) {
+            if (H == 0) {
+                H = 23;
+            } else {
+                H--;
+            }
+            M = M + 60 - 45;
+        } else {
+            M -= 45;
         }
-        else if( H> 0 && M > 45 ){
-            int c = H;
-            int d = M-45;
-            System.out.print(c + " " + d);
-        }
-        else if (H == 0 && M < 45 ){
-            int f = M+60-45;
-            System.out.print(23 + " " + f);
-        }
-        else if (H == 0 && M > 45){
-            int h = M-45;
-            System.out.print(H + " " + h);
-        }
-        // myObj.close();
+
+        System.out.println(H + " " + M);
+
+        myObj.close();
+
     }
 }
+// 처음에 내가 짠 코드
+// if( H > 0 && M < 45 ){
+// int a = H-1;
+// int b = M+60-45;
+// System.out.print(a + " " + b);
+// }
+// else if( H> 0 && M > 45 ){
+// int c = H;
+// int d = M-45;
+// System.out.print(c + " " + d);
+// }
+// else if (H == 0 && M < 45 ){
+// int f = M+60-45;
+// System.out.print(23 + " " + f);
+// }
+// else if (H == 0 && M > 45){
+// int h = M-45;
+// System.out.print(H + " " + h);
+// }
