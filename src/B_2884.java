6dcbb955c0fd1f9bad254e08960a66a6c59506e5
GabaeOh <rkqo4577@gmail.com>
2023-06-01 18:20:34 +0900

    qorwns

--- src/B_2884.java
+++ src/B_2884.java
@@ -3,25 +3,31 @@ import java.util.Scanner;
 public class B_2884 {
     public static void main(String[] args) {
         // refer : https://www.acmicpc.net/problem/2884
-        /* 문제 :  "45분 일찍 알람 설정하기" 원래 설정되어 있는 알람을 45분 앞서는 시간으로 바꾸는 것이다.*/
+        /* 문제 : "45분 일찍 알람 설정하기" 
+                 원래 설정되어 있는 알람을 45분 앞서는 시간으로 바꾸는 것이다.*/
 
         Scanner myObj = new Scanner(System.in);
         int H = myObj.nextInt();
         int M = myObj.nextInt();
 
-        if( M < 15 ){
-            System.out.print(H-1);
-            System.out.print(M+60-45);
+        if( H > 0 && M < 45 ){
+            int a = H-1;
+            int b = M+60-45;
+            System.out.print(a + " " + b);
         }
-        else if( M > 15 ){
-            System.out.print(H);
-            System.out.print(M-45);
+        else if( H> 0 && M > 45 ){
+            int c = H;
+            int d = M-45;
+            System.out.print(c + " " + d);
         }
-
-        else if (H == 0){
-            System.out.println(23);
-            System.out.println(M-45);
+        else if (H == 0 && M < 45 ){
+            int f = M+60-45;
+            System.out.print(23 + " " + f);
         }
-       
+        else if (H == 0 && M > 45){
+            int h = M-45;
+            System.out.print(H + " " + h);
+        }
+        // myObj.close();
     }
 }
